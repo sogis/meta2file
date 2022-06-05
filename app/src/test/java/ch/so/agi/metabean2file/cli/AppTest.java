4a6ae48a30c569b97ba17fcd07ec96cecb2c4dd6
edigonzales <edi.gonzales@gmail.com>
2022-06-05 16:45:12 +0200

    add app subproject

---
+++ app/src/test/java/ch/so/agi/metabean2file/cli/AppTest.java
@@ -0,0 +1,12 @@
+package ch.so.agi.metabean2file.cli;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+//    @Test 
+    public void appHasAGreeting() {
+//        App classUnderTest = new App();
+//        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+    }
+}
